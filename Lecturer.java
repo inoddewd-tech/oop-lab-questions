29d1f8d24e807e6bcf2b3d8b08d30af4102644c3
inoddewd-tech <inoddewd@gmail.com>
2026-04-10 19:34:15 +0530

    Add files via upload

--- Lecturer.java
+++ Lecturer.java
@@ -1,32 +1,25 @@
 
+package lab04;
+
 public class Lecturer extends StaffMember {
+
     private int courseCount;
     private double paymentPerCourse;
-    
-    public Lecturer(String staffID, String fullName, String department, 
-                    int courseCount, double paymentPerCourse) {
-        super(staffID, fullName, department);
+
+    public Lecturer(String fullName, String staffId, String department,
+                     int courseCount, double paymentPerCourse) {
+        super(fullName, staffId, department);
         this.courseCount = courseCount;
         this.paymentPerCourse = paymentPerCourse;
     }
-    
+
     
     public double calculateMonthlyPayment() {
         return courseCount * paymentPerCourse;
     }
-    
+
     public void displayLecturerDetails() {
         displayBasicDetails();
-        System.out.println("Course Count: " + courseCount);
-        System.out.println("Payment Per Course: $" + paymentPerCourse);
-        System.out.println("Monthly Payment: $" + calculateMonthlyPayment());
-    }
-}
-public class Lab04Main {
-    public static void main(String[] args) {
-        Lecturer lecturer1 = new Lecturer("L001", "Dr. John Smith", 
-                                          "Computer Science", 4, 2500.0);
-        lecturer1.displayLecturerDetails();
-        System.out.println("\nMonthly Payment: $" + lecturer1.calculateMonthlyPayment());
+        System.out.println("Courses: " + courseCount);
     }
 }
