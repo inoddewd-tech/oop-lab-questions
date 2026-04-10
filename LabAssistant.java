bc0b278db78ded019dc600aeb2cf0d16c119c67c
inoddewd-tech <inoddewd@gmail.com>
2026-04-10 18:50:43 +0530

    Add files via upload

---
+++ LabAssistant.java
@@ -0,0 +1,22 @@
+
+public class LabAssistant extends StaffMember {
+
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public LabAssistant(String fullName, String staffId, String department,
+                        int hoursWorked, double hourlyRate) {
+        super(fullName, staffId, department);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    public double calculateMonthlyPayment() {
+        return hoursWorked * hourlyRate;
+    }
+
+    public void displayLabAssistantDetails() {
+        displayBasicDetails();
+        System.out.println("Hours Worked: " + hoursWorked);
+    }
+}
