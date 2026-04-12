4214933a3ef6df4c13f87dec1d56ac4899ae0bc9
Dewdun De seram <inoddewd@gmail.com>
2026-04-12 10:48:34 +0530

    Add files via upload

--- Lab04Main.java
+++ Lab04Main.java
@@ -1,45 +1,51 @@
-package lab04;
-
 public class Lab04Main {
 
     public static void main(String[] args) {
 
+        // System name
         StaffMember.showSystemName();
 
+        // Policy
         UniversityPolicy.showPolicyHeader();
         System.out.println("University: " + UniversityPolicy.UNIVERSITY_NAME);
 
-        Lecturer l1 = new Lecturer("Nimal Perera", "L001", "IT", 3, 50000);
-        Lecturer l2 = new Lecturer("Kamal Silva", "L002", "CS", 2, 60000);
-        LabAssistant la1 = new LabAssistant("Saman Kumara", "A001", "IT", 120, 500);
+        
+        Lecturer lec1 = new Lecturer("Nimal Perera", "L001", "IT", 3, 50000);
+        Lecturer lec2 = new Lecturer("Kamal Silva", "L002", "CS", 2, 60000);
+        LabAssistant lab1 = new LabAssistant("Saman Kumara", "A001", "IT", 120, 500);
 
-        // Change department
-        l1.changeDepartment("Software Engineering");
+        
+        lec1.changeDepartment("Software Engineering");
 
-        double total = 0;
+        System.out.println("\n--- Lecturer 1 Details ---");
+        lec1.displayLecturerDetails();
+        double pay1 = lec1.calculateMonthlyPayment();
+        System.out.println("Monthly Payment: " + pay1);
+        System.out.println("Bonus: " + UniversityPolicy.calculateBonus(pay1));
 
-        System.out.println("\n--- Lecturer 1 ---");
-        l1.displayLecturerDetails();
-        double pay1 = l1.calculateMonthlyPayment();
-        System.out.println("Payment: " + pay1);
+        System.out.println("\n--- Lecturer 2 Details ---");
+        lec2.displayLecturerDetails();
+        double pay2 = lec2.calculateMonthlyPayment();
+        System.out.println("Monthly Payment: " + pay2);
+        System.out.println("Bonus: " + UniversityPolicy.calculateBonus(pay2));
 
-        System.out.println("\n--- Lecturer 2 ---");
-        l2.displayLecturerDetails();
-        double pay2 = l2.calculateMonthlyPayment();
-        System.out.println("Payment: " + pay2);
+        System.out.println("\n--- Lab Assistant Details ---");
+        lab1.displayLabAssistantDetails();
+        double pay3 = lab1.calculateMonthlyPayment();
+        System.out.println("Monthly Payment: " + pay3);
+        System.out.println("Bonus: " + UniversityPolicy.calculateBonus(pay3));
 
-        System.out.println("\n--- Lab Assistant ---");
-        la1.displayLabAssistantDetails();
-        double pay3 = la1.calculateMonthlyPayment();
-        System.out.println("Payment: " + pay3);
+        // Total payment
+        double total = pay1 + pay2 + pay3;
+        System.out.println("\nTotal Monthly Payment: " + total);
 
-        total = pay1 + pay2 + pay3;
+        // Staff count
+        System.out.println("Total Staff Count: " + StaffMember.getStaffCount());
 
-        System.out.println("\nTotal Payment: " + total);
-        System.out.println("Total Staff: " + StaffMember.getStaffCount());
-
-        l1.showCommonNotice();
-        l2.showCommonNotice();
-        la1.showCommonNotice();
+        // Common notice
+        System.out.println("\n--- Notices ---");
+        lec1.showCommonNotice();
+        lec2.showCommonNotice();
+        lab1.showCommonNotice();
     }
 }
